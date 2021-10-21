0f1047a03c0b079554cd94f492b8a22c2df5e4e5
faris <faris@mail.com>
2021-10-21 06:49:01 -0700

    feat: add post route to login :triangular_flag_on_post:

--- routes/login.js
+++ routes/login.js
@@ -6,4 +6,13 @@ router.get("/", (req, res, next) => {
    res.render("login", { title: "Login" });
 });
 
+router.post("/", (req, res, next) => {
+   const { email, password } = req.body;
+   if (email && password) {
+      return res.redirect("/");
+   }
+
+   res.redirect("/login");
+});
+
 module.exports = router;
