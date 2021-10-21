85afb5bd82f90948f53408b628333be050c9fede
faris <faris@mail.com>
2021-10-21 06:53:13 -0700

    feat: add post route to rregister  :triangular_flag_on_post:

--- routes/register.js
+++ routes/register.js
@@ -6,4 +6,13 @@ router.get("/", (req, res, next) => {
    res.render("register", { title: "Register" });
 });
 
+router.post("/", (req, res, next) => {
+   const { name, email, password } = req.body;
+   if (name && email && password) {
+      return res.redirect("/");
+   }
+
+   res.redirect("/register");
+});
+
 module.exports = router;
