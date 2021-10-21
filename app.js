3e2f1f7e650d4ca7a19a67a8c1cfff0f63b54f7a
faris <faris@mail.com>
2021-10-21 04:51:00 -0700

    feat: add login route :triangular_flag_on_post:

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 
 /* Routes Imports */
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const loginRouter = require("./routes/login");
 
 /* Create app */
 const app = express();
@@ -18,6 +18,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 /* Routes */
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/login", loginRouter);
 
 module.exports = app;
