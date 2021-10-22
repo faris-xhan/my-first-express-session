889be108276b334bd2e947b17533f57dceb1e7d2
faris <faris@mail.com>
2021-10-22 06:35:29 -0700

    build(dep): add login deps :heavy_plus_sign:

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 const path = require("path");
 const logger = require("morgan");
 const express = require("express");
-const expressEjsLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
 const loginRouter = require("./routes/login");
+const session = require("express-session");
 const registerRouter = require("./routes/register");
+const expressEjsLayouts = require("express-ejs-layouts");
 
 /* Create app */
 const app = express();
@@ -16,6 +17,16 @@ app.use(express.json());
 app.use(expressEjsLayouts);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+   session({
+      secret: "thisismysecretkeyidontshare",
+      resave: true,
+      saveUninitialized: true,
+      cookie: {
+         maxAge: 1000 * 60 * 60 * 24,
+      },
+   })
+);
 
 /* App Configs */
 app.set("view engine", "ejs");
